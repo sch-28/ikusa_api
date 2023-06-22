1628c86242d76cb80dc8f5450b580dd738394d07
sch-28 <jansch2803@googlemail.com>
2023-06-22 23:29:00 +0200

    fix: supbase warning

--- ikusa-api/src/util/db.ts
+++ ikusa-api/src/util/db.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 import { createClient } from "@supabase/supabase-js";
 export const prisma = new PrismaClient();
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY, {auth:{persistSession:false}});
