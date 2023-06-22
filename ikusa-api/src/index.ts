f17375128f02e3f6bb4644ade1475813c651b32c
sch-28 <jansch2803@googlemail.com>
2023-06-22 22:08:19 +0200

    feat: added cors

--- ikusa-api/src/index.ts
+++ ikusa-api/src/index.ts
@@ -1,18 +1,23 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import router from "./routes";
 import Logger from "./util/logger";
 
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(
+	cors({
+		origin: "https://ikusa.site",
+	})
+);
 
 const port = process.env.PORT;
 // Starts HTTP Server
 const server = app.listen(port, () => {
 	Logger.info(`Server started listening on: http://localhost:${port}`);
 });
-
 
 // Initializes the Routes
 app.use("/api", router);
