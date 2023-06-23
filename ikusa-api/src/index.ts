3d85dbf51af440e4819c162d32a08f1f4b0d6f9a
sch-28 <jansch2803@googlemail.com>
2023-06-24 01:07:33 +0200

    feat: added express-slow-down

--- ikusa-api/src/index.ts
+++ ikusa-api/src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import router from "./routes";
 import Logger from "./util/logger";
 import rateLimit from "express-rate-limit";
+import slowDown from "express-slow-down";
+
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
@@ -24,10 +26,19 @@ if (process.env.NODE_ENV === "production") {
 // Apply the rate limiting middleware to all requests
 app.use(
 	rateLimit({
-		windowMs: 60 * 1000, // 100 requests per minute
-		max: 100,
+		windowMs: 60 * 1000, // 1000 requests per minute
+		max: 1000,
 		standardHeaders: true,
 		legacyHeaders: false,
+	})
+);
+
+// Apply the speed limiter to all requests
+app.use(
+	slowDown({
+		windowMs: 60 * 1000, // 1000 requests per minute
+		delayAfter: 60,
+		delayMs: 1000,
 	})
 );
 
