635c475cc9d22eaabeab497e8073e4fcdd5b0004
sch-28 <jansch2803@googlemail.com>
2023-06-23 21:05:32 +0200

    feat: added rate limiter

--- ikusa-api/src/index.ts
+++ ikusa-api/src/index.ts
@@ -3,13 +3,23 @@ import express from "express";
 import cors from "cors";
 import router from "./routes";
 import Logger from "./util/logger";
-
+import rateLimit from "express-rate-limit";
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
 	cors({
 		origin: "https://ikusa.site",
+	})
+);
+
+// Apply the rate limiting middleware to all requests
+app.use(
+	rateLimit({
+		windowMs: 60 * 1000, // 100 requests per minute
+		max: 100,
+		standardHeaders: true,
+		legacyHeaders: false,
 	})
 );
 
